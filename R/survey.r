2b534c4c477b265d917ad1527ac2339c8de744ec
Clément Turbelin <clement.turbelin@gmail.com>
2019-11-28 10:37:54 +0100

    rename parameter "question" to variable in survey recoding functions, fix doc

--- R/survey.r
+++ R/survey.r
@@ -1,15 +1,12 @@
 ##
 # survey data managements
 # Functions dedicated to get surveys data and definitions from
-# an influenzaNet DB, following the plateform's definition
-# Each platform has a platform file, places in share/platform containing all platform specific variable definition and functions
+##
 
 #' aliases for symptom columns
 #'
 #' Aliases are meaningfull names for each columns of the survey data. InfluenzaNet surveys data are named using simple names (Q + number)
 #' but these names are not errorproof and are hard to memorize.
-#' epiwork.tables list defined in platform config file should describe the mapping between column names in the DB (Qxxx) and the column
-#' names used in R.
 #'
 #' These aliases are defined in the weekly survey definition in the platform file using
 #' @seealso \code{\link{platform_define_survey}}
@@ -21,7 +18,7 @@ get_symptoms_aliases <- function() {
   def$labels$symptoms
 }
 
-#' Convert column name (InfluenzaNet column names in the db) from (revert=F) and to variable aliases (revert=T)
+#' Convert DB column name (InfluenzaNet column names in the db) from (revert=F) and to question variable name (revert=T)
 #'
 #' Description of mapping are defined in the survey  by \code{\link{platform_define_survey}} in the platform file
 #' @param cols names of columns to rename
@@ -48,15 +45,15 @@ survey_default_language = function() {
   return(.Share$survey_default_language)
 }
 
-#' Return the "labels" for a given question
+#' Return the "labels" for a given question variable name
 #'
-#' If it is a multichoice question, it will return the names of columns containing all the responses choices for this question
+#' If it is a multichoice question, it will return the names of variable containing all the responses choices for this question
 #' If it is a single choice question, it will return a list of language independent labels (but human meaningful)
 #'
 #' These labels are defined in the survey  by \code{\link{platform_define_survey}} in the platform file
 #'
 #' @param survey name of the survey
-#' @param question name of the question, or if multichoice name of the question group (sometimes a more generic name)
+#' @param question name of the question variable, or if multichoice name of the question group (sometimes a more generic name)
 #' @return character vector of names
 #' @export
 survey_labels <- function(survey, question) {
@@ -70,27 +67,30 @@ survey_labels <- function(survey, question) {
 	labels
 }
 
-#' Get list of question aliases matching the given pattern (glob style)
+#' Get list of variable names matching the given pattern (glob style)
 #'
-#' These labels are defined in the survey  by \code{\link{platform_define_survey}} in the platform file
+#' These labels are defined in the survey by \code{\link{platform_define_survey}} in the platform file
 #'
 #' @param survey survey name
 #' @param pattern glob style pattern (ex "visit.*")
-#' @return character vector of questions
+#' @return character vector of questions variable names
 #' @export
 #' @importFrom utils glob2rx
 #'
 #' @examples
-#' \dontRun{
+#' \dontrun{
 #'  survey_questions_like("weekly", "visit.*") # All variables starting with visit.*
 #' }
 #'
-survey_questions_like <- function(survey, pattern) {
+survey_variables_like <- function(survey, pattern) {
   p = glob2rx(pattern)
   def = survey_definition(survey)
   n = names(def$aliases)
   n[grep(p, n)]
 }
+
+#' @rdname survey_variables_like
+survey_questions_like <- survey_variables_like
 
 #' Recode data from the DB storage code to the R labels (or to its translaction into default language)
 #'
@@ -98,18 +98,23 @@ survey_questions_like <- function(survey, pattern) {
 #' defined \code{\link{platform_define_survey}} usually in the platform file
 #'
 #' @param x values to recodes
-#' @param question name of the question (alias here)
+#' @param variable name of the question variable name to use for recoding
 #' @param survey name of the survey
 #' @param translate if TRUE, try to translate the labels (using i18n function @seealso i18n)
+#' @param question old parameter name for variable, for compatibility
 #' @return vector of recoded value in factor
 #' @export
-survey_recode <- function(x, question, survey, translate=F) {
+survey_recode <- function(x, variable, survey, translate=F, question=NULL) {
+
+  if(!is.null(question)) {
+    variable = question
+  }
 
   if(is.factor(x)) {
     rlang::abort("Cannot recode a factor, already recoded ?")
   }
 
-  recodes = survey_question_recoding(survey = survey, question=question, must.exists = TRUE)
+  recodes = survey_variable_recoding(survey = survey, question=question, must.exists = TRUE)
 
   if(is.null(labels)) {
     rlang::abort(sprintf("Unknown labels for question %s", question))
@@ -119,6 +124,10 @@ survey_recode <- function(x, question, survey, translate=F) {
 }
 
 #' Recode values with a given mapping
+#' @param x value to recode
+#' @param mapping list of mapping db value to label, label as name and db value as value
+#' @param translate if TRUE translate labels with \code{\link{i18n}}
+#' @return factor vector of recoded values
 recode_var <- function(x, mapping, translate=FALSE) {
   codes = as.vector(mapping)
   labels = names(mapping)
@@ -134,18 +143,18 @@ recode_var <- function(x, mapping, translate=FALSE) {
   factor(x, codes, labels)
 }
 
-#' Get the recoding mapping of a question in a survey
+#' Get the recoding mapping of a variable in a survey
 #'
 #' @param survey character survey name
-#' @param question question variable name
+#' @param name question variable name
 #'
 #' @return vector with label as name, database value as value
 #'
-survey_question_recoding <- function(survey, question, must.exists=TRUE) {
+survey_variable_recoding <- function(survey, name, must.exists=TRUE) {
   def = survey_definition(survey)
-  recodes = def$recodes[[ question ]]
+  recodes = def$recodes[[ name ]]
   if(is.null(recodes) && must.exists) {
-    rlang::abort(sprintf("Unknown codes for question %s", question))
+    rlang::abort(sprintf("Unknown codes for variable name %s", name))
   }
   recodes
 }
@@ -171,7 +180,7 @@ survey_single_table <- function(survey) {
 }
 
 #' Get survey definition
-#' Survey definition is a data structure of entries defining variable mapping (db name to variable name, recoding, variable sets)
+#' Survey definition is a data structure of entries defining variable mapping (db column name to variable name, recoding, variable sets)
 #' They are defined by \code{\link{platform_define_survey}} usually in the platform file
 #' @param survey survey name
 #' @return list parameters describing a survey
@@ -228,7 +237,6 @@ season.def = function(season, silent=F) {
 #' Get list of available season names
 #'
 #' @seealso \code{\link{concepts}}
-#'
 #'
 #' @return character vector of season names
 #' @export
