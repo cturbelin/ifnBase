e4ddc408d63f957522c641f2964838c1660906b5
Clément Turbelin <clement.turbelin@gmail.com>
2019-11-29 16:21:56 +0100

    simplify startup message

--- R/zzz.R
+++ R/zzz.R
@@ -11,14 +11,15 @@
   # Historical framework used variables defined in Globalenv
   # Import those variables variables from the GlobalEnv to create options entry
   imports = list(
-    "db_dsn"="DB_DSN",
-    "db_driver"="DB_DRIVER",
+    db_dsn="DB_DSN",
+    db_driver="DB_DRIVER",
     base.out.path = "OUT_PATH"
   )
 
-  # Already defined options
+  # Already defined options (using options(ifn=)) if any
   oo = get_option()
 
+  imported = c()
   for(i in seq_along(imports)) {
     name = names(imports[i])
     old.name = imports[[i]]
@@ -26,24 +27,27 @@
       old.name = name
     }
     if(is.null(oo[[name]]) && exists(old.name, envir=env)) {
-      packageStartupMessage(paste("Importing ", old.name," from global env"))
+      imported = c(imported, old.name)
       oo[[name]] <- get(old.name, envir = env)
     }
   }
-
   # BASE_PATH
   # In case of base path defined, it is assumed the workspace is organized
   # with a share/ directory holding
   # See vignettes workspace
   if(exists("BASE_PATH", envir = env)) {
     # If BASE_PATH exists, recreate workspace paths, as expected
-    packageStartupMessage("Importing BASE_PATH from global environment")
+    imported = c(imported, 'BASE_PATH')
     base.path = ending_slash(get("BASE_PATH", envir = env))
     oo$share.path = paste0(base.path, "share/")
     oo$share.lib.path = paste0(oo$share.path, "lib/")
     oo$platform.path = paste0(oo$share.path, "platform/")
     oo$share.cache = paste0(oo$share.path, "cache/")
     oo$share.data = paste0(oo$share.path, "data/")
+  }
+
+  if(length(imported)) {
+   packageStartupMessage(paste("Importing ", paste(sQuote(imported), collapse = ',')," from global environment"))
   }
 
   oo = merge_list(oo, defaults)
