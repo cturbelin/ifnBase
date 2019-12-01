cd44fe67d7c273f8cc32723988b2bcc42ce4731e
Clément Turbelin <clement.turbelin@gmail.com>
2019-12-01 10:11:11 +0100

    move bootstrap.R to tools/

---
+++ tools/bootstrap.R
@@ -0,0 +1,4 @@
+library(ifnBase)
+
+platform_define_survey("intake", survey_id = 9, table ="intake", template="eu:intake", mapping=list())
+platform_define_survey("weekly", survey_id = 8, table ="weekly", template="eu:weekly", mapping=list())
