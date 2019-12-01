a131945d163571e2dd338b655660286d2b0675d1
Clément Turbelin <clement.turbelin@gmail.com>
2019-12-01 12:05:21 +0100

    split season focused functions to seasons.R

---
+++ R/seasons.R
@@ -0,0 +1,54 @@
+
+#' Get historical season definition
+#'
+#' Each season should be described in the platform using \code{\link{platform_season_history}}
+#' historical.tables is a list with an entry for each season (season = year number of the first september in the season)
+#' \describe{
+#'  \item{intake}{table containing the intake survey data for the season}
+#'  \item{weekly}{table containing the weekly survey data for the season}
+#'  \item{health}{table/view containing the health status for each weekly for the season}
+#'  \item{year.pop}{population year to use}
+#'  \item{dates}{list(start, end), starting and ending date of the season}
+#' }
+#' This entries are defined
+#' @param season season definition to get
+#' @param silent boolean (not used.)
+#' @family seasons
+#' @return return the entry of config for the season (first year of the season)
+#' @export
+season_definition = function(season, silent=F) {
+  h = .Share$historical.tables[[as.character(season)]]
+  if( is.null(h) ) {
+    stop(paste("Unknown season", season,"in historical tables"))
+  }
+  h
+}
+
+#' @export
+#' @rdname season_definition
+season.def <- season_definition
+
+#' Get list of available season names
+#'
+#' @seealso \code{\link{concepts}}
+#'
+#' @family seasons
+#' @return character vector of season names
+#' @export
+get_historical_seasons = function() {
+  names(.Share$historical.tables)
+}
+
+
+#' Get Historical tables
+#'
+#' Historical tables is a list, with season year (first year of the season) as name and season definition
+#' @seealso Season in \code{\link{concepts}}
+#'
+#' @family seasons
+#' @seealso season.def
+#' @export
+get_historical_tables <- function() {
+  .Share$historical.tables
+}
+
