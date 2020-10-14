7feffd32f8da5ea609a12c3ba1b4bd8c0ad4f2b2
Clément Turbelin <clement.turbelin@gmail.com>
2020-10-14 17:27:16 +0200

    add constants for common symptoms

---
+++ R/symptoms.R
@@ -0,0 +1,34 @@
+#' Symptoms
+#'
+
+
+#' No symptome variable name
+#'
+#' To be used as a constant in the analysis
+#' @export
+NO_SYMPTOM = 'no.sympt'
+
+SYMPT_FEVER = 'fever'
+SYMPT_CHILLS = 'chills'
+SYMPT_RHINO = 'rhino'
+SYMPT_SNEEZE = 'sneeze'
+SYMPT_SORETHROAT = 'sorethroat'
+SYMPT_COUGH = 'cough'
+SYMPT_DYSPNEA = 'dyspnea'
+SYMPT_HEADACHE = 'headache'
+SYMPT_PAIN = 'pain'
+SYMPT_CHESTPAIN = 'chestpain'
+SYMPT_ASTHENIA = 'asthenia'
+SYMPT_ANOREXIA = 'anorexia'
+SYMPT_SPUTUM='sputum'
+SYMPT_WATERYEYE = 'wateryeye'
+SYMPT_NAUSEA = 'nausea'
+SYMPT_VOMITING = 'vomiting'='Q1_16'
+SYMPT_DIARRHEA = 'diarrhea'
+SYMPT_ABDOPAIN='abdopain'
+SYMPT_OTHER = 'sympt.other'
+SYMPT_SMELL = "loss.smell"
+SYMPT_TASTE = "loss.taste"
+SYMPT_NOSEBLEED = "nose.bleed"
+
+
