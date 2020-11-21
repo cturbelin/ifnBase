a902c0a83b07ad18a152f604b44e65517228fe23
Clément Turbelin <clement.turbelin@gmail.com>
2020-11-21 14:58:43 +0100

    refactor episode test (explore fail in travis)

--- tests/testthat/test_episode.R
+++ tests/testthat/test_episode.R
@@ -47,54 +47,64 @@ tests = list(
   )
 )
 
-test_that("episode_fusion.worst_strategy", {
+#strategy = list(vars=tests$strategies)
 
-  strategy = list(vars=tests$strategies)
+for(i in seq_along(tests$sets)) {
+  test = tests$sets[[i]]
 
-  for(i in seq_along(tests$sets)) {
-    test = tests$sets[[i]]
-    v = rlang::list2(!!test$name := test$values)
-    data = data.frame(list(person_id=i), episode=1, v)
+  test_that(paste0("episode_fusion.worst_strategy:",i), {
 
-    strategy.def = tests$strategies[[test$name]]
-    v$type = "worst"
-    if(!is.null(strategy.def$raw)) {
-      v[[test$name]] = raw_value(strategy.def$raw)
-    } else {
-      v[[test$name]] = factor(strategy.def$codes)
-    }
-    strategy = do.call(episode_strategy, v)
-    #str(strategy)
+      v = rlang::list2(!!test$name := test$values)
+      data = data.frame(list(person_id=i), episode=1, v)
 
-    r = ifnBase:::episode_fusion.worst_strategy(strategy, weekly=data, episode.column='episode')
-
-    result = r[[test$name]]
-    if(is.na(test$expected)) {
-      expect_equivalent(result, NA)
-    } else {
-      if(is.character(test$expected) && is.factor(result)) {
-        result = as.character(result)
-
+      strategy.def = tests$strategies[[test$name]]
+      v$type = "worst"
+      if(!is.null(strategy.def$raw)) {
+        v[[test$name]] = raw_value(strategy.def$raw)
+      } else {
+        v[[test$name]] = factor(strategy.def$codes)
       }
-      expect_equal(result, test$expected)
-    }
-    ## Try with recoded values
-    if(isTRUE(test$recode)) {
-      d = data
-      d[[test$name]] = survey_recode(d[[test$name]], variable = test$name, survey="weekly")
-      r = ifnBase:::episode_fusion.worst_strategy(strategy, weekly=d, episode.column='episode')
-      result = r[[test$name]]
-      expected = test$expected
 
-      if(is.na(expected)) {
+      strategy = do.call(episode_strategy, v)
+
+      r = try( ifnBase:::episode_fusion.worst_strategy(strategy, weekly=data, episode.column='episode'))
+
+      if(is(r, "try-error")) {
+        str(data)
+        str(strategy.def)
+        str(strategy)
+        fail(paste("Strategy failed"))
+      }
+
+
+      result = r[[test$name]]
+      if(is.na(test$expected)) {
         expect_equivalent(result, NA)
       } else {
-        expected = as.character(survey_recode(expected, variable = test$name, survey="weekly"))
-        result = as.character(result)
-        expect_equal(result, expected)
-      }
-    }
+        if(is.character(test$expected) && is.factor(result)) {
+          result = as.character(result)
 
-  }
-})
+        }
+        expect_equal(result, test$expected)
+      }
+      ## Try with recoded values
+      if(isTRUE(test$recode)) {
+        d = data
+        d[[test$name]] = survey_recode(d[[test$name]], variable = test$name, survey="weekly")
+        r = ifnBase:::episode_fusion.worst_strategy(strategy, weekly=d, episode.column='episode')
+        result = r[[test$name]]
+        expected = test$expected
+
+        if(is.na(expected)) {
+          expect_equivalent(result, NA)
+        } else {
+          expected = as.character(survey_recode(expected, variable = test$name, survey="weekly"))
+          result = as.character(result)
+          expect_equal(result, expected)
+        }
+      }
+
+  })
+
+}
 
